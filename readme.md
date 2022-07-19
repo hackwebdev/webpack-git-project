2a5b6f881ae319feeb76727b5f1d59a98c38cfa4
hackwebdev <hackwebdev@gmail.com>
2022-07-20 02:11:04 +0800

    initialize

---
+++ readme.md
@@ -0,0 +1,23 @@
+My Webpack Frontend Development Setup
+
+Install Webpack
+npm i -D webpack webpack-cli
+
+Install SASS
+npm i -D sass style-loader css-loader sass-loader
+
+Html Template
+npm i -D html-webpack-plugin
+
+Dev Server
+npm i -D webpack-dev-server
+
+Babel - a javascript transpiler that converts a newer version to old version
+npm i -D babel-loader @babel/core @babel/preset-env
+
+assets/ - for images
+styles/ - for scss files
+template.html - the main html file to edit
+
+$ npm run dev - for development
+$ npm run build - build for production
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name][contenthash].js',
+    clean: true,
+    assetModuleFilename: 'images/[name][ext]',
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+      directory: path.resolve(__dirname, './src/template.html'),
+    },
+    port: 9000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+        options: {},
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack App',
+      filename: 'index.html',
+      template: 'src/template.html',
+    }),
+  ],
+}
